08d9bffc2e80692a2ee95ea937ae04bf19855be5
Shriprama Rao <shriprama@gmail.com>
2019-01-27 21:30:23 -0600

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Shriprama Rao on 1/27/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
